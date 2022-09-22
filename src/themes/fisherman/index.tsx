962e116a6480362092763271af2f55b72cf45ce2
yangenze <yangenze@4399.com>
2022-09-22 15:33:31 +0800

    明前奶绿mua

--- src/themes/fisherman/index.tsx
+++ src/themes/fisherman/index.tsx
@@ -1,14 +1,14 @@
 // 钓鱼佬主题
 import React from 'react';
 import { Theme } from '../interface';
-import olk from '../fisherman/sounds/olk.mp3'
-import sorry from '../fisherman/sounds/sorry.mp3'
-import yddmyl from '../fisherman/sounds/一滴都没有了.mp3'
-import bkyo from '../fisherman/sounds/不可以哦.mp3'
-import nya from '../fisherman/sounds/关注明前奶绿nya.mp3'
-import white from '../fisherman/sounds/喜欢白丝.mp3'
+import ea from '../fisherman/sounds/呃啊.mp3'
+import e from '../fisherman/sounds/呃.mp3'
+import damie from '../fisherman/sounds/打咩.mp3'
+import nya from '../fisherman/sounds/nya.mp3'
+import white from '../fisherman/sounds/白丝.mp3'
+import guanzhu from '../fisherman/sounds/关注明前奶绿nya.mp3'
 import maolei from '../fisherman/sounds/猫雷.mp3'
-import chicken from '../fisherman/sounds/绿色肥鸡.mp3'
+import fish from '../fisherman/sounds/鱼人语.mp3'
 import night from '../fisherman/sounds/早安午安晚安.mp3'
 
 const imagesUrls = import.meta.glob('./images/*.png', {
@@ -17,36 +17,35 @@ const imagesUrls = import.meta.glob('./images/*.png', {
 });
 
 type SoundNames =
-    | 'olk'
-    | 'sorry'
-    | '一滴都没有了'
-    | '不可以哦'
-    | '关注明前奶绿nya'
-    | '喜欢白丝'
+    | '呃啊'
+    | '呃'
+    | '打咩'
+    | 'nya'
+    | '白丝'
     | '猫雷'
-    | '绿色肥鸡'
-    | '早安午安晚安';
+    | '鱼人语'
+    | '关注明前奶绿nya';
 
 const pictureSoundMap: Record<string, SoundNames> = {
-    ['olk']: 'olk',
-    ['sorry']: 'sorry',
-    ['不可以哦']: '不可以哦',
-    ['明前奶绿']: '关注明前奶绿nya',
-    ['白丝']: '喜欢白丝',
+    ['olk']: '呃',
+    ['不可以哦']: '打咩',
+    ['可爱小奶绿']: '呃啊',
+    ['明前奶绿']: 'nya',
+    ['白丝']: '白丝',
     ['终极猎手']: '猫雷',
-    ['坚果']: '绿色肥鸡',
+    ['坚果']: '鱼人语',
 };
 
 const sounds: { name: SoundNames; src: string }[] = [
-    { name: 'olk', src: olk },
-    { name: 'sorry', src: sorry },
-    { name: '一滴都没有了', src: yddmyl },
-    { name: '不可以哦', src: bkyo },
-    { name: '关注明前奶绿nya', src: nya },
-    { name: '喜欢白丝', src: white },
+    { name: '呃', src: e },
+    { name: '打咩', src: damie },
+    { name: '呃啊', src: ea },
+    { name: 'nya', src: nya },
+    { name: '白丝', src: white },
     { name: '猫雷', src: maolei },
-    { name: '绿色肥鸡', src: chicken },
-    { name: '早安午安晚安', src: night },
+    { name: '鱼人语', src: fish },
+    
+    { name: '关注明前奶绿nya', src: guanzhu },
 ];
 
 const mqnl = Object.entries(imagesUrls).map(([key, value]) => ({
@@ -62,7 +61,7 @@ export const mqnlTheme: Theme<SoundNames> = {
         name,
         content,
         clickSound: pictureSoundMap[name],
-        tripleSound: '早安午安晚安',
+        tripleSound: '关注明前奶绿nya',
     })),
     sounds,
 };
